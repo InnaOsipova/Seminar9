e98152db06303e14734e91078032ec4b8a522571
Inna Osipova <rutheva_inna@mail.ru>
2022-07-24 18:03:23 +0300

    Возведение в степень

---
+++ EX003/Program.cs
@@ -0,0 +1,9 @@
+﻿// два числа a и b  и должно быть а в степени b
+
+int StepenNamber (int a, int b)
+{
+    if (b <= 1) return a;
+    return a * (StepenNamber(a, b-1));
+}
+
+System.Console.WriteLine(StepenNamber(2, 2));
