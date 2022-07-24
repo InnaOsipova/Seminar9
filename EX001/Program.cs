fc1b635616bf0c4ae6d945d34fcce1c9e9d11b4a
Inna Osipova <rutheva_inna@mail.ru>
2022-07-24 17:12:34 +0300

    Рекурсия возвращает числа от 1 до n

---
+++ EX001/Program.cs
@@ -0,0 +1,13 @@
+﻿string SeriesOfNumbers (int a, int n)
+{
+    if (a == n) 
+    {
+        return n.ToString(); 
+    }
+    else 
+    {
+        return a.ToString() + " " + SeriesOfNumbers(a+1 , n); 
+    }
+}
+
+Console.Write(SeriesOfNumbers (1, 5));
